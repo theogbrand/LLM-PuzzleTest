33e0f9e1758f8262be97190c16f7528d4d6c596f
ob1-hopper <ob1-hopper@aisg.org>
2025-09-02 14:09:08 +0800

    add CUDA_VISIBLE_DEVICES

--- PuzzleVQA/evaluate.sh
+++ PuzzleVQA/evaluate.sh
@@ -2,6 +2,18 @@
 set -e
 
 MODEL_NAME=$1
+# Find number of available GPUs
+NUM_GPUS=$(nvidia-smi --list-gpus | wc -l)
+echo "Found $NUM_GPUS GPUs available"
+
+# Set CUDA_VISIBLE_DEVICES to use all available GPUs
+if [ $NUM_GPUS -gt 0 ]; then
+    CUDA_VISIBLE_DEVICES=$(seq -s, 0 $((NUM_GPUS-1)))
+    echo "Using GPUs: $CUDA_VISIBLE_DEVICES"
+else
+    echo "No GPUs found, running on CPU"
+    CUDA_VISIBLE_DEVICES=""
+fi
 
 # List of datasets
 DATASETS=(
@@ -30,7 +42,7 @@ DATASETS=(
 # Loop through each dataset and run the evaluation
 for DATA in "${DATASETS[@]}"; do
   echo "Evaluating dataset: $DATA with model: $MODEL_NAME"
-  python main.py evaluate_multi_choice data/${DATA}.json \
+  CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} python main.py evaluate_multi_choice data/${DATA}.json \
   --model_name ${MODEL_NAME} \
   --prompt_name cot_multi_extract
 done
